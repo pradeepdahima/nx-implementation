02ad4e6af693854e69eb956d915ae86bc974bf7e
pradeep.dahima <pradeep.dahima@gmail.com>
2021-12-29 18:49:43 +0530

    Adding nx implementation

--- apps/main/src/app/app.tsx
+++ apps/main/src/app/app.tsx
@@ -1,9 +1,12 @@
 import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
+import { Core, Button } from '@@orange-solutions/core';
 
 export function App() {
   return (
     <>
+      <Core />
+      <Button />
       <NxWelcome title="main" />
       <div />
     </>
